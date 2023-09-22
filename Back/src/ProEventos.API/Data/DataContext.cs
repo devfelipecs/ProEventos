ce5b2db125199bf91e875ea695c5bb92c572469e
Felipe <110936719+devfelipecs@users.noreply.github.com>
2023-09-22 19:19:11 -0300

    Adicionando DB
    Criando Banco de Dados SQLite3

--- Back/src/ProEventos.API/Data/DataContext.cs
+++ Back/src/ProEventos.API/Data/DataContext.cs
@@ -7,9 +7,8 @@ namespace ProEventos.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            
         }
 
-        public DbSet<Evento> ?Eventos { get; set; }
+        public DbSet<Evento>? Eventos { get; set; }
     }
 }
